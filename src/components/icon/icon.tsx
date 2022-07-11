b3d38c2762118ebd247aec4e9d1a70179ffe3d1b
imendoza <dev@ivanmendoza.net>
2022-07-11 16:28:24 -0600

    [0.6.1-topic-selector]
    MODIFIED
    * 🛠️ Improvements: TopicSelector component rebuilt from scratch to match with the design.

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -2,13 +2,14 @@ import './icon.css';
 import './icons.css';
 
 type IconProps = {
+  className?: string;
   name: string;
   size?: '12' | '16' | '24' | '32' | '48' | '64' | '72' | '96' | '128';
 };
 
-const Icon: React.FC<IconProps> = ({ name, size }) => {
+const Icon: React.FC<IconProps> = ({ className, name, size }) => {
   const iconSize = size ? size : '24';
-  return <div className={`hn-icon hn-icon-${name} hn-icon-size-${iconSize}`} />;
+  return <div className={`hn-icon hn-icon-${name} hn-icon-size-${iconSize} ${className ? className : ''}`} />;
 };
 
 export default Icon;
